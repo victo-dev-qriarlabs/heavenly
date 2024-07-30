30e04704b5739ccc2672f5c5a77cc060b97e6aa5
josiel <josielufopa@gmail.com>
2024-07-30 18:12:22 -0300

    style: correcao da quebra de css

--- heavenly/src/view/components/Duvidas/TextDuvidas.js
+++ heavenly/src/view/components/Duvidas/TextDuvidas.js
@@ -2,32 +2,32 @@ import React from 'react';
 
 const TextDuvidas = () => {
   return (
-    <div className='mt-12'>
-      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-72 w-[400px]'>
+    <div className='overflow-hidden mt-12'>
+      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-72 w-11/12'>
         <h1 className='text-2xl font-extrabold mb-5 text-white text-center mt-7 text-bold'>O que é um Horóscopo?</h1>
         <p className='text-justify mb-6 text-xs text-white'>
           Um horóscopo é uma previsão astrológica baseada na posição dos astros, como o Sol, a Lua e os planetas, no momento do seu nascimento. Ele oferece percepções sobre sua personalidade e possíveis eventos ou tendências na sua vida diária, semanal ou mensal. Os horóscopos são frequentemente usados para entender melhor as influências astrais em várias áreas da vida, como amor, trabalho e saúde.
         </p>
       </div>
-      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto my-2 w-[400px]'>
+      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto my-2 w-11/12'>
         <h1 className='text-2xl font-extrabold mb-5 text-white text-center mt-7 text-bold'>Qual meu signo do Zodiaco ?</h1>
         <p className='text-justify mb-6 text-xs text-white'>
         Você pode descobrir seu signo do zodíaco com base na sua data de nascimento. O zodíaco é dividido em doze signos, cada um cobrindo um período específico do ano. Por exemplo, se você nasceu entre 21 de março e 19 de abril, seu signo é Áries. Se nasceu entre 20 de abril e 20 de maio, seu signo é Touro. Cada signo tem características únicas que influenciam a personalidade e o comportamento.
         </p>
       </div>
-      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-2 w-[400px]'>
+      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-2 w-11/12'>
         <h1 className='text-2xl font-extrabold mb-5 text-white text-center mt-7 text-bold'>Os horóscopos são verdadeiros ?</h1>
         <p className='text-justify mb-6 text-xs text-white'>
         Muitas pessoas acreditam que horóscopos podem fornecer conselhos valiosos e insights sobre suas vidas. No entanto, é importante lembrar que os horóscopos são guias gerais baseados em padrões astrológicos. Eles não são previsões exatas e podem variar de pessoa para pessoa. A melhor maneira de usar um horóscopo é como uma ferramenta de reflexão e orientação, não como uma previsão definitiva.
         </p>
       </div>
-      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-2 w-[400px]'>
+      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-2 w-11/12'>
         <h1 className='text-2xl font-extrabold mb-5 text-white text-center mt-7 text-bold'>O que é um ascendente ?</h1>
         <p className='text-justify mb-6 text-xs text-white'>
         O ascendente é o signo do zodíaco que estava ascendendo no horizonte no momento do seu nascimento. Para descobrir seu ascendente, você precisa saber a hora exata e o local do seu nascimento. O ascendente influencia a maneira como você se apresenta ao mundo e como os outros o percebem. Ele pode complementar e até modificar algumas características do seu signo solar, oferecendo uma visão mais completa da sua personalidade.
         </p>
       </div>
-      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-2 w-[400px]'>
+      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-2 w-11/12'>
         <h1 className='text-2xl font-extrabold mb-5 text-white text-center mt-7 text-bold'>Horoscopo preve meu futuro ?</h1>
         <p className='text-justify mb-6 text-xs text-white'>
         Os horóscopos podem fornecer orientações sobre tendências e energias que podem estar presentes na sua vida em determinados períodos. Eles podem ajudar a identificar momentos favoráveis para tomar decisões ou enfrentar desafios. No entanto, os horóscopos não devem ser considerados previsões detalhadas de eventos específicos. Eles são melhor utilizados como ferramentas de orientação e reflexão para ajudar você a navegar pelas influências astrais em sua vida.
--- heavenly/src/view/components/MenuNav.js
+++ heavenly/src/view/components/MenuNav.js
@@ -4,18 +4,19 @@ import Logo from "../assets/logo.jpg";
 function MenuNav() {
   return (
     <>
-      <div className="fixed top-0 w-full z-50 bg-verde2">
+      <div className="fixed left-0 top-0 w-full z-50 bg-verde2">
         <div className="container mx-auto flex justify-between items-center p-6 px-6 lg:px-0 relative">
           <input className="peer hidden" type="checkbox" name="teste" id="teste" />
-          <label className="peer-checked:hamburguer block cursor-pointer lg:hidden peer-checked:border-2 peer-checked:border-verde1 p-3 rounded-md transition-all flex flex-col items-center justify-center"
+          <label className="peer-checked:hamburguer lg:hidden peer-checked:border-2 peer-checked:border-verde1 p-3 rounded-md transition-all flex flex-col items-center justify-center"
             htmlFor="teste">
-            <div className="w-6 h-0.5 rounded bg-verde1 transition-all duration-300 mb-1"></div>
-            <div className="w-6 h-0.5 rounded bg-verde1 transition-all duration-300 mb-1"></div>
-            <div className="w-6 h-0.5 rounded bg-verde1 transition-all duration-300"></div>
+            <div className="w-7 h-0.5 rounded bg-verde1 transition-all duration-300 mb-1"></div>
+            <div className="w-7 h-0.5 rounded bg-verde1 transition-all duration-300 mb-1"></div>
+            <div className="w-7 h-0.5 rounded bg-verde1 transition-all duration-300"></div>
           </label>
 
-          <div className="absolute inset-0 flex items-center justify-center text-lg font-bold uppercase text-white" style={{ zIndex: -10, pointerEvents: 'none' }}>
-            <img src={Logo} className='w-16 rounded-full'/>
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-lg font-bold " style={{ zIndex: -10, pointerEvents: 'none' }}>
+            <img src={Logo} className='w-8 rounded-full'/>
+            <h1 className='text-white'>Heavenly</h1>
           </div>
 
           <div className="-translate-x-full peer-checked:translate-x-0 lg:translate-x-0 inset-0 fixed lg:static pt-20
