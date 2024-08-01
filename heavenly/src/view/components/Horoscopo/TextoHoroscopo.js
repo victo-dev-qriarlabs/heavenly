be09e0d2cb22225bf9b7c6eee11b0c515c624de4
Francisco Marconi <franciscomarconi19@gmail.com>
2024-08-01 15:14:19 -0300

    feat: - criacao de rota para consumo da api. - exclusão de pasta e arquivo teste de comunicacao com api. - montagem do componente de horoscopo diario de cada signo.

--- heavenly/src/view/components/Horoscopo/TextoHoroscopo.js
+++ heavenly/src/view/components/Horoscopo/TextoHoroscopo.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-const TextoHoroscopo = ({ signo }) => {
+const TextoHoroscopo = ({ titulo,data,texto }) => {
+  const hasData = titulo && data && texto;
   return (
-    <div className='overflow-hidden mt-12'>
-      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-72 w-full]'>
-        <h1 className='text-2xl font-extrabold mb-5 text-white text-center mt-7 text-bold'>Horóscopo diário de {signo}</h1>
-        <p className='text-justify mb-6 text-xs text-white'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse Acillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <hr className="my-4 border-t-2 border-gray-300 " />
-        <img src="/img/ExemploImg.png" alt="exemplo" className="w-full h-auto" />
-        <p className='mt-4 text-justify text-xs mb-8 text-white'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p className='mt-4 text-justify text-xs mb-8 text-gray-700  text-center text-white'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+    <div className='mt-12'>
+      <div className='bg-verde2 p-6 rounded-lg shadow-lg w-full md:w-3/4 mx-auto mt-72 w-11/12'>
+        {hasData ? (
+          <>
+            <h1 className='text-3xl font-bold mb-4 text-white mt-4'>{titulo}</h1>
+            <p className='text-white mb-6 text-sm'>{data}</p>
+            <hr className="my-4 border-t-2 border-gray-300" />
+            <p className='mt-4 text-justify text-white mb-6 text-sm'>{texto}</p>
+          </>
+        ) : (
+          <p className='text-white mb-6 text-sm'>Estamos enfrentando problemas internos. Tente novamente mais tarde.</p>
+        )}
       </div>
     </div>
   );
