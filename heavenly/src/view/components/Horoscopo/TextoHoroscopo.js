0cde8710743f0fa8a0b8f262546989a1a9d9746b
josiel <josielufopa@gmail.com>
2024-08-01 10:02:58 -0300

    style: Fiz a adaptação do banner para desktop

--- heavenly/src/view/components/Horoscopo/TextoHoroscopo.js
+++ heavenly/src/view/components/Horoscopo/TextoHoroscopo.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const TextoHoroscopo = ({ signo }) => {
   return (
-    <div className='mt-12'>
-      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-72 w-[400px]'>
+    <div className='overflow-hidden mt-12'>
+      <div className='bg-verde2 p-6 shadow-lg md:w-3/4 mx-auto mt-72 w-full]'>
         <h1 className='text-2xl font-extrabold mb-5 text-white text-center mt-7 text-bold'>Horóscopo diário de {signo}</h1>
         <p className='text-justify mb-6 text-xs text-white'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse Acillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- heavenly/src/view/components/MenuNav.js
+++ heavenly/src/view/components/MenuNav.js
@@ -4,17 +4,16 @@ import Logo from "../assets/logo.jpg";
 function MenuNav() {
   return (
     <>
-      <div className="fixed left-0 top-0 w-full z-50 bg-verde2">
+      <div className="fixed left-0 top-0 w-full z-50 bg-verde2 h-20">
         <div className="container mx-auto flex justify-between items-center p-6 px-6 lg:px-0 relative">
-          <input className="peer hidden" type="checkbox" name="teste" id="teste" />
+          <input className="peer hidden" type="checkbox" name="transicao" id="transicao" />
           <label className="peer-checked:hamburguer lg:hidden peer-checked:border-2 peer-checked:border-verde1 p-3 rounded-md transition-all flex flex-col items-center justify-center"
-            htmlFor="teste">
-            <div className="w-7 h-0.5 rounded bg-verde1 transition-all duration-300 mb-1"></div>
-            <div className="w-7 h-0.5 rounded bg-verde1 transition-all duration-300 mb-1"></div>
-            <div className="w-7 h-0.5 rounded bg-verde1 transition-all duration-300"></div>
+            htmlFor="transicao">
+            <div className="w-7 h-0.5 rounded bg-verde1 mb-1"></div>
+            <div className="w-7 h-0.5 rounded bg-verde1 mb-1"></div>
+            <div className="w-7 h-0.5 rounded bg-verde1 "></div>
           </label>
-
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-lg font-bold " style={{ zIndex: -10, pointerEvents: 'none' }}>
+<div className="absolute inset-0 flex flex-col items-center justify-center text-lg font-bold z-[-10] pointer-events-none">
             <img src={Logo} className='w-8 rounded-full'/>
             <h1 className='text-white'>Heavenly</h1>
           </div>
